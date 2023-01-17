ee7e35f9ca2eef44fcd5008bebbc0d4bdd684c43
LTMX <nicolasreinhard.pro@gmail.com>
2023-01-17 19:08:32 +0100

    Refactor, Packaged

--- Primitives/Primitives.cs
+++ Primitives/Primitives.cs
@@ -9,23 +9,25 @@ public class Primitives : MonoBehaviour
     private const string smoothIcosphere = "Smooth Icosphere";
     private const string smoothCylinder = "Smooth Cylinder";
     private const string torus = "Torus";
+    private const string meshFileExtension = ".fbx";
+    private const string path = "GameObject/3D Object/";
 
 
-    [MenuItem("GameObject/3D Object/Cone")]
+    [MenuItem(path + "Cone")]
     public static void SpawnCone() => SpawnBaseObject(cone).AddMeshCollider().SetConvex();
 
-    [MenuItem("GameObject/3D Object/Create Smooth Cube")]
+    [MenuItem(path + "Smooth Cube")]
     public static void SpawnSmoothCube() => SpawnBaseObject(smoothCube).AddColliderAdaptive<BoxCollider>();
 
-    [MenuItem("GameObject/3D Object/Create Smooth Icosphere")]
+    [MenuItem(path + "Smooth Icosphere")]
     public static void SpawnSmoothIcosphere() => SpawnBaseObject(smoothIcosphere).AddMeshCollider().SetConvex();
 
-    [MenuItem("GameObject/3D Object/Create Smooth Cylinder")]
+    [MenuItem(path + "Smooth Cylinder")]
     public static void SpawnSmoothCylinder() => SpawnBaseObject(smoothCylinder).AddMeshCollider().SetConvex();
 
-    [MenuItem("GameObject/3D Object/Create Torus")]
+    [MenuItem(path + "Torus")]
     public static void SpawnTorus() => SpawnBaseObject(torus).AddColliderAdaptive<MeshCollider>();
     
-    private static GameObject SpawnBaseObject(string name) => NewGameObject(name).AddMesh(name).AddMaterial().SetParent().SetAllSettings();
+    private static GameObject SpawnBaseObject(string name) => NewGameObject(name).AddMesh(name + meshFileExtension).AddMaterial().SetParent().ApplySettings();
     
 }
--- Primitives/PrimitivesUtils.cs
+++ Primitives/PrimitivesUtils.cs
@@ -6,22 +6,18 @@ using Object = UnityEngine.Object;
 public static class PrimitivesUtils
 {
     
-    private const string primitivesSettings = "Primitives Settings";
+    private const string primitivesSettings = "Primitives Settings.asset";
+    private const string packageDataPath = "Packages/com.ltmx.primitives/Primitives/Objects/";
     
-    public static GameObject AddMesh(this GameObject go, string name)
-    {
-        go.AddComponent<MeshFilter>().mesh = Load<Mesh>(name);
-        return go;
-    }
-    
+    public static GameObject AddMesh(this GameObject go, string name) => go.AddComponent<MeshFilter>()?.LoadMesh(name).gameObject;
+
     /// Adds Material From Path
     public static GameObject AddMaterial(this GameObject go, string name)
     {
         //Check if Renderer is Already active
-        var g = go.GetComponent<MeshRenderer>();
+        var g = go.GetComponent<MeshRenderer>(); 
         if (g != null) g.material = Load<Material>(name);
         else go.AddComponent<MeshRenderer>().material = Load<Material>(name);
-
         return go;
     }
     
@@ -29,10 +25,9 @@ public static class PrimitivesUtils
     public static GameObject AddMaterial(this GameObject go)
     {
         //Check if Renderer is Already active
-        var g = go.GetComponent<MeshRenderer>();
+        var g = go.Get<MeshRenderer>();
         if (g != null) g.material = LoadPrimitivesSettings().material;
         else go.AddComponent<MeshRenderer>().material = LoadPrimitivesSettings().material;
-
         return go;
     }
     
@@ -50,80 +45,54 @@ public static class PrimitivesUtils
     public static GameObject AddSphereCollider(this GameObject go) => go.Add<SphereCollider>();
     public static GameObject AddCapsuleCollider(this GameObject go) => go.Add<SphereCollider>();
     
-    public static GameObject SetConvex(this GameObject go)
-    {
-        var g = go.GetComponent<MeshCollider>();
-        if(g != null) g.convex = true;
-        return go;
-    }
+
 
     public static GameObject AddColliderAdaptive<T>(this GameObject go) where T : Component
     {
-        switch (LoadPrimitivesSettings().colliderType)
+        return LoadPrimitivesSettings().colliderType switch
         {
-            case PrimitivesSettings.ColliderType.Adaptive:
-                return go.Add<T>();
-            case PrimitivesSettings.ColliderType.MeshConvex:
-                return go.AddMeshCollider().SetConvex();
-            case PrimitivesSettings.ColliderType.Mesh:
-                return go.AddMeshCollider();
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
+            PrimitivesSettings.ColliderType.Adaptive => go.Add<T>(),
+            PrimitivesSettings.ColliderType.MeshConvex => go.AddMeshCollider().SetConvex(),
+            PrimitivesSettings.ColliderType.Mesh => go.AddMeshCollider(),
+            _ => throw new ArgumentOutOfRangeException()
+        };
     }
-    
-    
-    public static GameObject AddRigidBody(this GameObject go)
-    {
-        return go.Add<Rigidbody>();
-    }
-    
-    public static GameObject UseGravity(this GameObject go, bool state)
-    {
-        var g = go.GetComponent<Rigidbody>();
-        if(g != null) g.useGravity = state;
-        return go;
-    }
+
     
     public static GameObject SetGravityFromSettings(this GameObject go)
     {
-        var g = go.GetComponent<Rigidbody>();
+        var g = go.Get<Rigidbody>();
         if(g != null) g.useGravity = LoadPrimitivesSettings().UseGravity;
         return go;
     }
     
     
     
-    public static GameObject SetAllSettings(this GameObject go)
+    public static GameObject ApplySettings(this GameObject go)
     {
         var s = LoadPrimitivesSettings();
-        var material = go.GetComponent<Material>();
-
         if (s.material != null) go.AddMaterial(s.material);
         if (s.AddRigidbody) go.AddRigidBody().UseGravity(s.UseGravity);
-
-
         return go;
     }
     
-    
-    
-    
-    // Shortcut For Resources.Load("Primitives" + name)
-    public static T Load<T>(string name) where T : Object => Resources.Load<T>("Primitives/" + name);
+    public static T Load<T>(string name) where T : Object => AssetDatabase.LoadAssetAtPath<T>(packageDataPath + name);
 
     public static PrimitivesSettings LoadPrimitivesSettings() => Load<PrimitivesSettings>(primitivesSettings);
 
     //Creates A New GameObject
-    public static GameObject NewGameObject(string name) => new GameObject(name);
+    public static GameObject NewGameObject(string name) => new(name);
 
-    //adds component and returns the GameObject
-    public static GameObject Add<T>(this GameObject go) where T : Component
-    {
-        go.AddComponent<T>();
-        return go;
-    }
     
+    public static MeshFilter LoadMesh(this MeshFilter mf, string name)
+    {
+        mf.mesh = Load<Mesh>(name);
+        return mf;
+    }
+}
+
+public static class PrimitiveCreationExtension
+{
     //Sets The gameObject's parent according to the selection
     public static GameObject SetParent(this GameObject go)
     {
@@ -132,4 +101,29 @@ public static class PrimitivesUtils
         
         return go;
     }
+    
+    //adds component and returns the GameObject
+    public static GameObject Add<T>(this GameObject go) where T : Component
+    {
+        go.AddComponent<T>();
+        return go;
+    }
+    public static T Get<T>(this GameObject go) where T : Component => go.GetComponent<T>();
+    
+    public static GameObject UseGravity(this GameObject go, bool state) => go.Get<Rigidbody>().SetGrapity(state).gameObject;
+    public static GameObject AddRigidBody(this GameObject go) => go.Add<Rigidbody>();
+    public static GameObject SetConvex(this GameObject go) => go.Get<MeshCollider>()?.SetConvex().gameObject;
+
+    public static MeshCollider SetConvex(this MeshCollider m)
+    {
+        m.convex = true;
+        return m;
+    }
+    public static Rigidbody SetGrapity(this Rigidbody r, bool state)
+    {
+        r.useGravity = state;
+        return r;
+    }
+    
+    
 }
